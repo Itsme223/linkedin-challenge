9a49a687a5fdcf64a95bb1958586d41c2a2e7381
tevinph <t.pilgrim23@hotmail.com>
2024-04-06 12:55:31 -0400

    update

---
+++ .vscode/data.js
@@ -0,0 +1,17 @@
+let userProfile = {
+    `username`: "Tevin Pilgrim-Hampden";
+    `profilePicture`:"Software Engineer";
+    `jobTitle`:"./assets/images/headshot.jpg";
+}
+
+const loadUserProfile => (userProfile)
+
+.onload = function() {
+    loadUserProfile(userProfile, onload);
+  };
+
+let createPost() = (onclick) => {
+    
+    const newInfo = document.createTextNode
+
+}
--- .vscode/scripts/index.js
+++ .vscode/scripts/index.js
@@ -1,17 +1,35 @@
-const username = "Tevin Pilgrim-Hampden";
-const jobtitle = "Software Engineer";
-const profilePic = "./assets/images/headshot.jpg";
+// const username = "Tevin Pilgrim-Hampden";
+// const jobtitle = "Software Engineer";
+// const profilePic = "./assets/images/headshot.jpg";
 
+const loadUserProfile() = {
 const usernameElems = document.querySelectorAll('.data-username');
 const jobTitleElm = document.querySelector(".data-jobtitle");
 const profilPicElm = document.querySelectorAll(".data-profilePic");
 
 for (let el of usernameElems) {
-    el.innerHTML = username;
+    el.innerHTML = userProfile.username;
 }
 
-jobTitleElem.innerHTML = jobTitle;
+jobTitleElem.innerHTML = userProfile.jobTitle;
 
-for( let profilePic){
-    pro.src = profilePic;
+for( let pro of profilePicElems){
+    pro.src = userProfile.profilePic;
 }
+
+const createPost = () => {
+    const usernameElem = document.querySelector(".data-username").innerHTML;
+    const post = document.querySelector(".data-post").value;
+    
+    if (post) {
+        const parentDiv =document.querySelector(".posts");
+        //Create a div,with class = posts__item
+        const postsItemDiv = document.createElement("div");
+        postsItemDiv.classList.add("posts__item");
+    }
+    const parentDiv =document
+    const newPost = document.createElement("div");
+    newPost.classList.add('posts__item')
+}
+
+};
