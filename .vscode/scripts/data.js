867adb6ead28ab9f8a43975022280694c60caa55
tevinph <t.pilgrim23@hotmail.com>
2024-04-09 18:45:32 -0400

    post update

--- .vscode/scripts/data.js
+++ .vscode/scripts/data.js
@@ -2,17 +2,5 @@
 const userProfile = {
     username: "Tevin Pilgrim-Hampden",
     jobTitle: "Software Engineer",
-    profilePic: "./assets/images/headshot.jpg",
+    profilePic: "./assets/images/headshot.jpg"
 };
-
-// const loadUserProfile = (userProfile)
-
-// .onload = function() {
-//     loadUserProfile(userProfile, onload);
-//   };
-
-// let createPost() = (onclick) => {
-    
-//     const newInfo = document.createTextNode
-
-// }
